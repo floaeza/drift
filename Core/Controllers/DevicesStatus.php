28b9ae9e0eee1a103aca1d21664d003d3719bb79
Tania Maldonado <tmaldonado@bbinco.com>
2021-04-05 10:45:30 -0600

    Actualizacion para consultar el estatus de los dispositivos 3

--- Core/Controllers/DevicesStatus.php
+++ Core/Controllers/DevicesStatus.php
@@ -24,19 +24,22 @@ switch ($Option){
         // DEVICES ON
             $OffDevices = $DevicesData->getDevicesByStatus('POWER_OFF');
             $DevicesCount = $DevicesData->getOperatingDevices();
-            array_push($Status,array('DevicesOn' => $DevicesCount));
 
         // DEVICES OFF
             $DevicesCount = $DevicesData->getDevicesByStatus('POWER_OFF');
-            array_push($Status,array('DevicesOff' => $DevicesCount));
 
         // HDMI CONNECTED
             $HdmiConnected = $DevicesData->getDevicesHdmi('1');
-            array_push($Status,array('HdmiConnected' => $HdmiConnected));
 
         // HDMI CONNECTED
             $HdmiDisconnected = $DevicesData->getDevicesHdmi('0');
-            array_push($Status,array('HdmiDisconnected' => $HdmiDisconnected));
+
+            array_push($Status,array(
+                'DevicesOn' => $DevicesCount,
+                'HdmiDisconnected' => $HdmiDisconnected,
+                'DevicesOff' => $DevicesCount,
+                'HdmiConnected' => $HdmiConnected
+            ));
 
         $Response = $Status;
         break;
