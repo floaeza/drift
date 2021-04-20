72768c125435085b216aba833c6b58526c93a41a
Tania Maldonado <tmaldonado@bbinco.com>
2021-04-20 12:09:41 -0600

    Actualizacion checkrecordings

--- Views/Layouts/Tv/Scripts/TvSquareRecorder.js
+++ Views/Layouts/Tv/Scripts/TvSquareRecorder.js
@@ -1933,30 +1933,37 @@ function CheckRecordings(){
             Debug(ProgramStartHour + ' '+ProgramStartMinute);
             Debug(ProgramEndHour + ' '+ProgramEndMinute);
 
-            if(ProgramEndHour === '00' && parseInt(ProgramEndMinute) > 0){
-
-                Debug(ChannelsJson[REC_CHNL_POS].DTNU);
-
-                var dateString = ProgramYear+'-'+ProgramMonth+'-'+ProgramDay;
-
-                Debug(dateString);
-                var myDate = new Date(dateString);
-                Debug(myDate);
-                //add a day to the date
-                myDate.setDate(myDate.getDate() + 2);
-
-                //add a day to the date
-                Debug(myDate);
-
-
+            if(ProgramEndHour === '00' && parseInt(ProgramEndMinute) >= 0){
+                // Debug('--------------------------------------->>1.1');
+                // Debug(ChannelsJson[REC_CHNL_POS].DTNU);
+                //
+                // var dateString = ProgramYear+'-'+ProgramMonth+'-'+ProgramDay;
+                //
+                // Debug(dateString);
+                // var myDate = new Date(dateString);
+                // Debug(myDate);
+                // //add a day to the date
+                // myDate.setDate(myDate.getDate() + 2);
+                //
+                // //add a day to the date
+                // Debug(myDate);
             }
 
-
-
             ProgramUtcStartDate = Date.UTC(ProgramYear, (ProgramMonth -1), ProgramDay, ProgramStartHour, ProgramStartMinute);
-            ProgramUtcEndDate = Date.UTC(ProgramYear, (ProgramMonth -1), ProgramDay, ProgramEndHour, ProgramEndMinute);
+
             Debug('--------------------------------------->>2');
             Debug(ProgramUtcStartDate);
+
+            if(parseInt(REC_PROG_POS) === LastProgramsPositions[RowSelected]){
+                Debug('--------------------------------------->>2.1');
+                var ProgramSeconds   = ChannelsJson[REC_CHNL_POS].PROGRAMS[REC_PROG_POS].MNTS * 60;
+
+                ProgramUtcEndDate = ProgramUtcStartDate + ProgramSeconds;
+            } else {
+                Debug('--------------------------------------->>2.2');
+                ProgramUtcEndDate = Date.UTC(ProgramYear, (ProgramMonth -1), ProgramDay, ProgramEndHour, ProgramEndMinute);
+            }
+
             Debug(ProgramUtcEndDate);
 
             ProgramUtcStartDate = ProgramUtcStartDate / 1000;
