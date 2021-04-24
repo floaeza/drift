97c37408a81d98fa54a8712a8bc145d605661c60
Tania Maldonado <tmaldonado@bbinco.com>
2021-04-24 11:33:31 -0600

    Actualizacion version por cliente

--- Core/Models/Libraries.php
+++ Core/Models/Libraries.php
@@ -1,10 +1,10 @@
 <?php
 
-$GetJson    = !empty($_POST['GetJson']) ? $_POST['GetJson'] : false;
+$GetJson    = !empty($_POST['GetJson']) ? $_POST['GetJson'] : true;
 
 if($GetJson == true){
-    require_once '/Core/Models/Database.php';
-    require_once '/Core/DataAccess/Config.php';
+    require_once 'Core/Models/Database.php';
+    require_once 'Core/DataAccess/Config.php';
     $ConfigData  = new Config('system','Libraries');
     $Client = $ConfigData->getConfigByName('Identifier').'/';
 }
