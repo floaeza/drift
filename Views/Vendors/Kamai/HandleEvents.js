74de8e0a1c2c033ce45e8522c878857c002cee3f
Tania Maldonado <tmaldonado@bbinco.com>
2021-04-29 13:01:37 -0600

    Actualizacion PVR

--- Views/Vendors/Kamai/HandleEvents.js
+++ Views/Vendors/Kamai/HandleEvents.js
@@ -265,11 +265,16 @@ Debug('---------->>>');
     var StorageInfo = [];
         StorageInfo = ENTONE.recorder.getStorageInfo();
 
+        Debug('>>>> freeSpace: '+StorageInfo.freeSpace);
+        Debug('>>>> freeSpace: '+StorageInfo.totalSpace);
         //freeSpace
         //totalSpace
 
     var assi = ENTONE.recorder.getAssetInfo('0000000217');
 
+         Debug('>>>> freeSpace: '+StorageInfo.duration);
+         Debug('>>>> freeSpace: '+StorageInfo.size);
+//duration
 
     Debug(JSON.stringify(assi));
 
