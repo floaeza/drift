5fed28f32f92dc8a40c358ed83cff7ea5450add3
Tania Maldonado <tmaldonado@bbinco.com>
2021-04-30 23:56:02 -0600

    Actualizacion EPG y PVR

--- Core/Models/Libraries.php
+++ Core/Models/Libraries.php
@@ -15,6 +15,8 @@ if($GetJson == true){
     $EpgLogo = $ConfigData->getConfigByName('EpgLogo');
 
     $JsonLibraries['EpgLogo'] = '/BBINCO/TV/Media/Logos/'.$Client.$EpgLogo;
+} else {
+    $Client = '';
 }
 
 $ServerIp = !empty($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '';
