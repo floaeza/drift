ce9907e351cb9b13221cfaedd94472cd34ec8ffb
Tania Maldonado <tmaldonado@bbinco.com>
2021-05-03 16:54:16 -0600

    Actualizacion EPG y PVR

--- Views/Vendors/Kamai/Player.js
+++ Views/Vendors/Kamai/Player.js
@@ -210,7 +210,7 @@
 
             DurationAsset = parseInt(Duration,10) * 60;
             Debug('>>>>>> DurationAsset: '+DurationAsset);
-            PositionAsset = (PositionInfo.playPosition)/1000;
+            PositionAsset = Math.round((PositionInfo.playPosition)/1000);
 
             Debug('>>>>>> PositionAsset: '+PositionAsset);
             
