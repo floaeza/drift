3d0a4b87c9ece7219c0794eb930a198e59600137
Tania Maldonado <tmaldonado@bbinco.com>
2021-05-06 17:02:48 -0600

    Actualizacion EPG y PVR

---
+++ Views/Scripts/CHL/Commands.js
@@ -0,0 +1,46 @@
+
+function Red(){
+    location.reload(true);
+}
+
+function Blue(){
+
+}
+
+function Green(){
+
+}
+
+function Yellow(){
+
+}
+
+function Close(){
+    if(CurrentModule === 'Tv'){
+        TvClose();
+    } else if(CurrentModule === 'Menu'){
+        //
+    } else if(CurrentModule === 'Movies'){
+        VodClose();
+    } else if(CurrentModule === 'Moods'){
+        MoodsClose();
+    }
+}
+
+function Back(){
+    if(CurrentModule === 'Tv'){
+        TvClose();
+    } else if(CurrentModule === 'Menu'){
+        //
+    } else if(CurrentModule === 'Movies'){
+        VodClose();
+    } else if(CurrentModule === 'Moods'){
+        MoodsClose();
+    }
+}
+
+function Menu(){
+    if(CurrentModule !== 'Menu' && Device['Services']['ActiveMenu'] === true){
+        GoPage('menu.php', Device['MenuId'], 'Menu');
+    }
+}
