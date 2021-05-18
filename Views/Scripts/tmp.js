a770d16961a3e07684ed5d5d087a1481c5d569b2
Tania Maldonado <tmaldonado@bbinco.com>
2021-05-18 09:20:51 -0600

    Actualizacion EPG y PVR

---
+++ Views/Scripts/tmp.js
@@ -0,0 +1,30 @@
+###############################################################################
+# Options for INFORMIR option space                                           #
+###############################################################################
+
+    option space Infomir;
+
+option Infomir.autostart         code 1 = text;
+option Infomir.bootargs          code 2 = text;
+option Infomir.mcip              code 3 = ip-address;
+option Infomir.mcport            code 4 = integer 16;
+option Infomir.oppubfile         code 9 = text;
+option Infomir.mcip_img          code 10 = ip-address;
+option Infomir.mcport_img        code 11 = integer 16;
+option Infomir.mcip_mng          code 12 = ip-address;
+option Infomir.mcport_mng        code 13 = integer 16;
+option Infomir.ip_log            code 14 = ip-address;
+option Infomir.port_log          code 15 = integer 16;
+option Infomir.logo_x            code 16 = integer 16;
+option Infomir.logo_y            code 17 = integer 16;
+option Infomir.bg_color          code 18 = integer 32;
+option Infomir.fg_color          code 19 = integer 32;
+option Infomir.VerNumber         code 20 = text;
+option Infomir.DateTime          code 21 = text;
+option Infomir.portal_dhcp       code 22 = text;
+option Infomir.timezone          code 23 = text;
+option Infomir.update_url        code 24 = text;
+option Infomir.update_sboot      code 25 = text;
+option Infomir.update_ver        code 26 = text;
+option Infomir.update_mode       code 27 = text;
+option Infomir.update_sboot_ver  code 28 = text;
