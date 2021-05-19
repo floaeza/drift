1f399a16c42b5678882aa0b9e8d65361187b535f
Tania Maldonado <tmaldonado@bbinco.com>
2021-05-19 16:16:59 -0600

    Actualizacion EPG y PVR

--- Admin/Core/Querys/Member.php
+++ Admin/Core/Querys/Member.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+//session_start();
 /* *****************************************************************************
  * OBJETIVO: Recibe del controlador de locaciones la opcion a ejecutar [Select, Insert, Update,
  * Delete] y regresa un mensaje dependiendo de la accion
