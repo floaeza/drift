c2af118ebcb30cf64958123b67b1f3287b47df70
Tania Maldonado <tmaldonado@bbinco.com>
2021-05-21 16:07:51 -0600

    Actualizacion EPG y PVR

--- Views/Vendors/Kamai/Player.js
+++ Views/Vendors/Kamai/Player.js
@@ -40,7 +40,7 @@
 
         StopVideo();
 
-        Video.open(Source + CheckPort);
+        Video.open(Source + CheckPort, null, { sync:0 });
 
         //ret = video.open(url, null, {pltbuf: 3600});
         //, null, {sync:0,seek_to_start:1}
