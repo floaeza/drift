bcdbd61c079c89c4343fed6f2db5845ff26725ea
Tania Maldonado <tmaldonado@bbinco.com>
2021-05-25 09:22:31 -0600

    Actualizacion EPG y PVR

--- menu.php
+++ menu.php
@@ -82,7 +82,7 @@
 
             // Libreria y estilo del tema asignado al dispositivo
             $Header->set('LayoutStyle', $Libraries['MenuStyles'].$ModuleInfo['opcion_template'].'.css' );
-        $Header->set('ThemeStyle', $Libraries['ThemesPath'].$ModuleInfo['opcion_template'].'/'.$TvTheme);
+            $Header->set('ThemeStyle', $Libraries['ThemesPath'].$TvTheme);
 
             // Aigna variable general del modulo
             $Header->set('CurrentModule', $CurrentModule);
