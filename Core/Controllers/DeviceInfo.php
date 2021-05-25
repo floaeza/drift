330569f0078b89d34fb47532ba485b30806cdada
Tania Maldonado <tmaldonado@bbinco.com>
2021-05-25 11:10:35 -0600

    Actualizacion EPG y PVR

--- Core/Controllers/DeviceInfo.php
+++ Core/Controllers/DeviceInfo.php
@@ -13,14 +13,14 @@ $MacAddress    = !empty($_POST['MacAddress']) ? $_POST['MacAddress'] : '';
 
 $DevicesData  = new Devices($MacAddress, $CurrentController);
 
-$EventString = !empty($_POST['EventString']) ? $_POST['EventString'] : '';
-$EventHdmi   = !empty($_POST['EventHdmi']) ? $_POST['EventHdmi'] : '';
+$EventString = !empty($_POST['EventString']) ? $_POST['EventString'] : '-';
+$EventHdmi   = !empty($_POST['EventHdmi']) ? $_POST['EventHdmi'] : '1';
 //$EventNetman = !empty($_POST['EventNetman']) ? $_POST['EventNetman'] : '';
-$LastChannel = !empty($_POST['LastChannel']) ? $_POST['LastChannel'] : '';
+$LastChannel = !empty($_POST['LastChannel']) ? $_POST['LastChannel'] : 'TV';
 $CurrentDate = !empty($_POST['CurrentDateStb']) ? $_POST['CurrentDateStb'] : '';
-$DeviceId    = !empty($_POST['DeviceId']) ? $_POST['DeviceId'] : '';
+$DeviceId    = !empty($_POST['DeviceId']) ? $_POST['DeviceId'] : '1';
 
-$Reboot = 'false';
+$Reboot = '';
 
 $DeviceUpdate =  array (
     'mensaje_evento'   => $EventString,
