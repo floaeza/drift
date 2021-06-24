ad9246ef49205708e1ef5b7dd6ff10446c8cd7fb
floaeza <floaeza@bbinco.com>
2021-06-24 11:45:29 -0600

    epg

--- Views/Scripts/Tv.js
+++ Views/Scripts/Tv.js
@@ -116,9 +116,9 @@
         /* Si tiene activa EPG actualiza la variable que por defecto tiene el valor de general */
         if(Device['Services']['ActiveEpg'] === true){
              //if(MacAddress === '00:00:00:00:00:01'){
-                 SourceEpgFile = Libraries['EpgDaysPath'] + 'epg_demo.json';
+                //  SourceEpgFile = Libraries['EpgDaysPath'] + 'epg_demo.json';
              // } else {
-             //     SourceEpgFile = Libraries['EpgDaysPath'] + 'epg_' + CurrentDate + '_' + Device['Services']['PackageId'] + '.json';
+                 SourceEpgFile = Libraries['EpgDaysPath'] + 'epg_' + CurrentDate + '_' + Device['Services']['PackageId'] + '.json';
              // }
             Debug('------- SetEpgFile ->>> SourceEpgFile: ' + SourceEpgFile);
             GetJsonEpg();
