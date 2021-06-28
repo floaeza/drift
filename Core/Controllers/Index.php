9913b2892d8e098bd8d07b383d41bfc96f151650
Tania Maldonado <tmaldonado@bbinco.com>
2021-06-28 10:46:56 -0600

    Actualizacion BBINCOTV

--- Core/Controllers/Index.php
+++ Core/Controllers/Index.php
@@ -11,7 +11,7 @@
     
     $CurrentController = 'IndexController';
 
-    echo $Option     = !empty($_POST['Option']) ? $_GET['Option'] : 'r';
+    echo $Option     = !empty($_POST['Option']) ? $_POST['Option'] : 'r';
     $MacAddress = !empty($_POST['MacAddress']) ? $_GET['MacAddress'] : '';
     $IpAddress  = !empty($_POST['IpAddress']) ? $_POST['IpAddress'] : '';
     $Firmware   = !empty($_POST['Firmware']) ? $_POST['Firmware'] : '';
