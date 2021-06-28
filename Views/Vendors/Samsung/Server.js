bb42fa41e7ddfbb8ee5364f51934ca400201af21
Tania Maldonado <tmaldonado@bbinco.com>
2021-06-28 09:10:29 -0600

    Actualizacion BBINCOTV

--- Views/Vendors/Samsung/Server.js
+++ Views/Vendors/Samsung/Server.js
@@ -1,6 +1,6 @@
 /* !! IMPORTANTE, se debe cambiar antes de general el WGT !! */
 
-var ServerIp = '172.16.0.15';
+var ServerIp = '10.0.3.10';
 var ServerSource = 'http://'+ServerIp+'/BBINCO/TV/';
 console.log(ServerIp);
 console.log(ServerSource);
