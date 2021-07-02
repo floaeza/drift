ccdf7c632cdafe2e8a938138fde23518b72fe199
Tania Maldonado <tmaldonado@bbinco.com>
2021-07-02 09:38:02 -0600

    Actualizacion BBINCOTV

---
+++ Views/Vendors/Samsung/HandleEvents.js
@@ -0,0 +1,38 @@
+
+var listener = {
+    onbufferingstart: function() {
+        Debug('Buffering start.');
+    },
+
+    // Se actualiza cada segundo, genera demasiados logs
+    // onbufferingprogress: function(percent) {
+    //     Debug('Buffering progress data : ' + percent);
+    // },
+
+    onbufferingcomplete: function() {
+        Debug('Buffering complete.');
+    },
+    onstreamcompleted: function() {
+        Debug('Stream Completed');
+    },
+
+    // Se actualiza cada segundo, genera demasiados logs
+    // oncurrentplaytime: function(currentTime) {
+    //     Debug('Current playtime: ' + currentTime);
+    // },
+
+    onerror: function(eventType) {
+        Debug('event type error : ' + eventType);
+    },
+
+    onevent: function(eventType, eventData) {
+        Debug('event type: ' + eventType + ', data: ' + eventData);
+    },
+
+    onsubtitlechange: function(duration, text, data3, data4) {
+        Debug('subtitleText: ' + text);
+    },
+    ondrmevent: function(drmEvent, drmData) {
+        Debug('DRM callback: ' + drmEvent + ', data: ' + drmData);
+    }
+};
--- Views/Vendors/Samsung/Player.js
+++ Views/Vendors/Samsung/Player.js
@@ -18,48 +18,8 @@ var WindowMaxWidth  = 0,
     WindowMinWidth  = 0,
     WindowMinHeight = 0;
 
-var listener = {
-    onbufferingstart: function() {
-        Debug('Buffering start.');
-    },
-
-    // Se actualiza cada segundo, genera demasiados logs
-    // onbufferingprogress: function(percent) {
-    //     Debug('Buffering progress data : ' + percent);
-    // },
-
-    onbufferingcomplete: function() {
-        Debug('Buffering complete.');
-    },
-    onstreamcompleted: function() {
-        Debug('Stream Completed');
-    },
-
-    // Se actualiza cada segundo, genera demasiados logs
-    // oncurrentplaytime: function(currentTime) {
-    //     Debug('Current playtime: ' + currentTime);
-    // },
-
-    onerror: function(eventType) {
-        Debug('event type error : ' + eventType);
-    },
-
-    onevent: function(eventType, eventData) {
-        Debug('event type: ' + eventType + ', data: ' + eventData);
-    },
-
-    onsubtitlechange: function(duration, text, data3, data4) {
-        Debug('subtitleText: ' + text);
-    },
-    ondrmevent: function(drmEvent, drmData) {
-        Debug('DRM callback: ' + drmEvent + ', data: ' + drmData);
-    }
-};
-
 var Player = webapis.avplay;
     Player.setListener(listener);
-
-
 
 /* *****************************************************************************
  * Reproductor de canal
