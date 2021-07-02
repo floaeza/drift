c0445491be12a1458a6e01d849e52e5801fe18ce
Tania Maldonado <tmaldonado@bbinco.com>
2021-07-02 09:00:05 -0600

    Actualizacion BBINCOTV

--- Views/Vendors/Samsung/Player.js
+++ Views/Vendors/Samsung/Player.js
@@ -92,7 +92,7 @@ function PlayDigitalChannel(Source){
         Player.close();
         Player.open(Source);
         Player.setDisplayRect(0, 0, 1920, 1080);
-        //Player.setListener(listener);
+        Player.setListener(listener);
         Player.prepareAsync(function() {
             Player.play();
         });
@@ -238,3 +238,39 @@ function AssetStatus(Duration){
 
     }
 }
+
+var listener = {
+    onbufferingstart: function() {
+        Debug('Buffering start.');
+    },
+
+    onbufferingprogress: function(percent) {
+        Debug('Buffering progress data : ' + percent);
+    },
+
+    onbufferingcomplete: function() {
+        Debug('Buffering complete.');
+    },
+    onstreamcompleted: function() {
+        Debug('Stream Completed');
+    },
+
+    oncurrentplaytime: function(currentTime) {
+        Debug('Current playtime: ' + currentTime);
+    },
+
+    onerror: function(eventType) {
+        Debug('event type error : ' + eventType);
+    },
+
+    onevent: function(eventType, eventData) {
+        Debug('event type: ' + eventType + ', data: ' + eventData);
+    },
+
+    onsubtitlechange: function(duration, text, data3, data4) {
+        Debug('subtitleText: ' + text);
+    },
+    ondrmevent: function(drmEvent, drmData) {
+        Debug('DRM callback: ' + drmEvent + ', data: ' + drmData);
+    }
+};
