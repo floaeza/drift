f2f130f66e24a3da4ea1464cb06b139b9b0f3f93
floaeza <floaeza@bbinco.com>
2021-07-05 13:05:30 -0600

    standby

--- Views/Scripts/VPL/Commands.js
+++ Views/Scripts/VPL/Commands.js
@@ -19,6 +19,48 @@ function Green(){
 }
 
 function Yellow(){
+    alert('pltbuf');
+var video;
+var url = doh._testParameters.pltv.videoUrl;
+var original_settings = {};
+var ret = ENTONE.resource.init();
+video = new ENTONE.video();
+doh.setCentralizedVideoCallback("main", video);
+
+
+var deferred = new doh.Deferred();
+var ret;
+
+ret = ENTONE.resource.restoreVideoPlayer();
+doh.assertNotEqual(0, ret);
+var players = ENTONE.resource.getVideoPlayers();
+video = players[0].player;
+doh.assertTrue(video);
+
+doh.setVideoCallback("main", function(e, vp, h){
+    if (e === ENTONE.video.VIDEOEVENT_PLTV_BUFFER_START){
+        // Wait for 5 seconds to get enough PLTV buffer
+        setTimeout(function(){
+            ret = video.play(0);
+            doh.assertEqual(ENTONE.status.SUCCESS, ret);
+        }, 5000);
+        
+        // Pause 10 seconds
+        setTimeout(function(){
+            deferred.callback(true);
+        }, 15000);
+    }
+}, this);
+
+ret = video.open(url, null, {pltbuf: 3600});
+doh.assertEqual(ENTONE.status.SUCCESS, ret);
+
+ret = video.setVideoPosition(100, 100, 720, 480, 1);
+
+ret = video.play();
+doh.assertEqual(ENTONE.status.SUCCESS, ret);
+
+return deferred;
 
 }
 
