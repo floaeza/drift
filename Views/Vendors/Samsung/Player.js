a6026044092442311ffc59175ba7a0bb404b0045
Tania Maldonado <tmaldonado@bbinco.com>
2021-07-06 15:28:38 -0600

    Actualizacion BBINCOTV

--- Views/Vendors/Samsung/Player.js
+++ Views/Vendors/Samsung/Player.js
@@ -25,9 +25,6 @@ var WindowMaxWidth  = 0,
 
     Debug('PLAYER WEBAPIS.AVPLAY > LISTENER');
 
-var current_dt = tizen.time.getCurrentDateTime();
-alert("current date/time is " + current_dt.toLocaleString());
-
 /* *****************************************************************************
  * Reproductor de canal
  * ****************************************************************************/
