92f9b628268aa4a0e123cb35f1ad258d65fbf93f
tmaldonado@bbinco.com <tmaldonado@bbinco.com>
2021-08-17 12:33:40 -0600

    Actualizacion BBINCOTV

--- Views/Vendors/Samsung/Server.js
+++ Views/Vendors/Samsung/Server.js
@@ -1,5 +1,5 @@
 /* !! IMPORTANTE, se debe cambiar antes de general el WGT !! */
 
 var ServerIp = '187.141.142.149:9001';
-var ServerFolder = '/BBINCO_BETA/TV/'
+var ServerFolder = '/BBINCO_BETA/TV/';
 var ServerSource = 'http://'+ServerIp+ServerFolder;
