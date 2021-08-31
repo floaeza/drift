1e1990d057a7f25d8be3554a92ce65e59ed31a91
tmaldonado@bbinco.com <tmaldonado@bbinco.com>
2021-08-31 17:06:03 -0600

    Actualizacion BBINCOTV

--- Core/Controllers/reboot_interface.py
+++ Core/Controllers/reboot_interface.py
@@ -1,3 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: ascii -*-
+import getpass
+import telnetlib
+import os
+    
+HOST = "10.0.3.144"
+user = "root"
+password = "root2root"
 
+tn=telnetlib.Telnet(HOST)
+tn.write(b'root\n')
+tn.write(b'root2root\n')
+tn.write(b"ls\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
