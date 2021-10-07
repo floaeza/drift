9439e72848865f469165bffbb8fa15a7bfb148b6
tmaldonado@bbinco.com <tmaldonado@bbinco.com>
2021-10-07 11:01:41 -0600

    Actualizacion BBINCOTV

--- index2.php
+++ index2.php
@@ -10,7 +10,13 @@
     
     //var big_mac = 'ASTB.GetMacAddress()';
     
-    </script>";
+    </script>
+    <style> 
+    body{
+        margin:0; padding:0; display:block; width:100%; border:none;
+    }
+    </style>
+    ";
     
     echo "<iframe id='maind' src='index.php' height='100%' frameborder=0 marginwidth=0 marginheight=0 onload='this.contentWindow.focus()' style= 'margin:0; padding:0; display:block; width:100%; border:none;'></iframe>";
     //echo "<frameset>";
