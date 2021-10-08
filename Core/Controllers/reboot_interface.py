fad82cf834df9f850ce9fb42c8c6eceac84c6e33
tmaldonado@bbinco.com <tmaldonado@bbinco.com>
2021-10-08 08:56:14 -0600

    Actualizacion BBINCOTV

--- Core/Controllers/reboot_interface.py
+++ Core/Controllers/reboot_interface.py
@@ -9,7 +9,7 @@ t=xtelnet.session()
 hora = datetime.now().strftime('%H')
 print(int(hora))
 payload = {'Option': 'GetAminos'}
-Devices = requests.post('http://172.22.22.10/BBINCO/TV/Core/Controllers/DevicesStatus.php', data=payload)
+Devices = requests.post('http://localhost/BBINCO/TV/Core/Controllers/DevicesStatus.php', data=payload)
 IDF = json.loads(Devices.content)
 print(IDF)
 for ips in IDF:
@@ -18,7 +18,7 @@ for ips in IDF:
         #ip = '10.30.12.134'
         mac = ips['mac_address']
         payload = {'Option': 'SetKillProcess', 'MacAddress':mac, 'Kill':1}
-        Devices = requests.post('http://172.22.22.10/BBINCO/TV/Core/Controllers/DevicesStatus.php', data=payload)
+        Devices = requests.post('http://localhost/BBINCO/TV/Core/Controllers/DevicesStatus.php', data=payload)
         t.connect(ip, username='root',password='root2root',p=23,timeout=8)
         if int(hora) > 16:
             output1=t.execute('ps')
--- Views/Vendors/Samsung/Server.js
+++ Views/Vendors/Samsung/Server.js
@@ -2,6 +2,6 @@
 // var ServerIp = '187.141.142.149:9001';
 // var ServerFolder = '/BBINCO/TV';
 
-var ServerIp = '172.22.22.10';
+var ServerIp = '10.0.3.10';
 var ServerFolder = '/BBINCO/TV';
 var ServerSource = 'http://'+ServerIp+ServerFolder+'/';
