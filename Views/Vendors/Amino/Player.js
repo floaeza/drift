e9c13dd6bcc418db65e3d583d4d4856cecbb318d
floaeza <floaeza@bbinco.com>
2021-10-29 11:48:57 -0600

    Player-Amino

--- Views/Vendors/Amino/Player.js
+++ Views/Vendors/Amino/Player.js
@@ -28,8 +28,6 @@
     
     function PlayChannel(Source, Port, ProgramIdChannnel, ProgramIdPosition, AudioPid){
         
-        Debug('Playchannel-------->>>***'+ProgramIdChannnel);
-        
         var CheckPort = '',
             CheckProgram = '';
         
@@ -37,11 +35,18 @@
                 CheckPort = ':' + Port;
             }
 
-            if(ProgramIdChannnel){
-                 CheckProgram = ';Progid='+ProgramIdChannnel+';audiopid='+AudioPid;
-                //CheckProgram = ';Progid='+ProgramIdChannnel;
-            }
+Debug('########################### Channelinfo: '+ProgramIdChannnel);
 
+            if(ProgramIdChannnel){
+            //  CheckProgram = ';Progid='+ProgramIdChannnel+';audiopid='+AudioPid;
+	     //	CheckProgram = ';Progid='+ProgramIdChannnel;
+	      if(AudioPid!=null){
+                    CheckProgram = ';Progid='+ProgramIdChannnel+';audiopid='+AudioPid;
+                }else{
+                    CheckProgram = ';Progid='+ProgramIdChannnel;
+                }
+            }
+	  Debug('########################### Channelinfo: '+CheckProgram);
         // Detiene el proceso de la reproduccion anterior
         StopVideo();
 
@@ -139,16 +144,15 @@
  * Obtiene los tamanos maximos y minimos de la pantalla
  * ****************************************************************************/
 
-function GetWindowFullSize(){
-    WindowMaxWidth   = window.screen.width;
-    WindowMaxHeight  = window.screen.height;
-}
-
-function GetWindowMinSize(){
-    WindowMinWidth   = ((window.screen.width)*33)/100;
-    WindowMinHeight  = ((window.screen.height)*33)/100;
-}
-
+    function GetWindowFullSize(){
+        WindowMaxWidth   = window.screen.width;
+        WindowMaxHeight  = window.screen.height;
+    }
+    
+    function GetWindowMinSize(){
+        WindowMinWidth   = ((window.screen.width)*33)/100;
+        WindowMinHeight  = ((window.screen.height)*33)/100;
+    }
 
 /* *****************************************************************************
  * Funcion para poner TV en pantalla completa
@@ -163,7 +167,7 @@ function GetWindowMinSize(){
  * ****************************************************************************/
     
     function MinimizeTV(){
-        windowTV.SetRectangle((13.5*WindowMaxWidth)/100, (5*WindowMaxWidth)/100, WindowMinWidth, WindowMinHeight);
+        windowTV.SetRectangle('15', '60', WindowMinWidth, WindowMinHeight);
     }
     
 /* *****************************************************************************
