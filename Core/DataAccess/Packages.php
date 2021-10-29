038333ba0909d53680548d17cf998b555b104877
floaeza <floaeza@bbinco.com>
2021-10-29 12:44:22 -0600

    PACKAGES

--- Core/DataAccess/Packages.php
+++ Core/DataAccess/Packages.php
@@ -41,7 +41,7 @@ class Packages extends Database {
                                       "canales ON paquete_canal.id_canal = canales.id_canal",
                                       "estaciones ON canales.id_estacion = estaciones.id_estacion",
                                       "",
-                                      "","id_paquete = '$PackageId'  AND canal_activo = '1' ","","","canales.numero_canal ASC");
+                                      "","id_paquete = '$PackageId'  AND canal_activo = '1' ","","","paquete_canal.numero_canal ASC");
         $this->PackageListById = $this->getResult();
 
         $this->disconnect();
@@ -176,7 +176,7 @@ class Packages extends Database {
                                       "canales ON paquete_canal.id_canal = canales.id_canal",
                                       "estaciones ON canales.id_estacion = estaciones.id_estacion",
                                       "",
-                                      "","id_paquete = '$PackageId'","","","canales.numero_canal ASC");
+                                      "","id_paquete = '$PackageId'","","","paquete_canal.numero_canal ASC");
         $this->PackageListById2 = $this->getResult();
 
         $this->disconnect();
