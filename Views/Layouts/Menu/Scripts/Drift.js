a754a2e10e8c053f913ec181ab50360a8dce17bb
floaeza <floaeza@bbinco.com>
2021-11-03 16:39:21 -0700

    package2

--- Views/Layouts/Menu/Scripts/Drift.js
+++ Views/Layouts/Menu/Scripts/Drift.js
@@ -1,7 +1,8 @@
 // @ts-nocheck
 var MenuDate        = document.getElementById('MenuDate'),
     MenuHour        = document.getElementById('MenuHour');
-    alert('ready');
+    
 
 $(document).ready(function() {
+    alert('ready');
 });
