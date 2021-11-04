07e34388ae513e540f5bcaafa94e03926b082633
floaeza <floaeza@bbinco.com>
2021-11-03 17:20:15 -0700

    package

--- Views/Layouts/Menu/Scripts/Drift.js
+++ Views/Layouts/Menu/Scripts/Drift.js
@@ -3,5 +3,45 @@ var MenuDate        = document.getElementById('MenuDate'),
     MenuHour        = document.getElementById('MenuHour');
 
 $(document).ready(function() {
-    alert('ready')
+    initial();
 });
+
+function initial(){
+    GetWeather();
+}
+
+function MenuSelect(Direction){
+    switch (Direction) {
+        case 'RIGHT':
+            alert('RIGHT');
+            break;
+    
+        case 'LEFT':
+            alert('LEFT');
+            break;
+        case 'UP':
+            alert('UP');
+            break;
+    
+        case 'DOWN':
+            alert('DOWN');
+            break;
+    }
+
+}
+
+function MenuRight(){
+    MenuSelect('RIGHT');
+}
+
+function MenuLeft(){
+    MenuSelect('LEFT');
+}
+
+function MenuDown(){
+    MenuSelect('DOWN');
+}
+
+function MenuUp(){
+    MenuSelect('UP');
+}
