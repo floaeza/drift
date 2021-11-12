9b45e13d7764dad958fcc5a10775ee98212fbddb
[BBsalazar] <[bsalazar@bbinco.com]>
2021-11-12 13:32:54 -0700

    up

--- Views/Layouts/Contents/Scripts/VideoOnDemand.js
+++ Views/Layouts/Contents/Scripts/VideoOnDemand.js
@@ -379,11 +379,12 @@ if(MoviePanelFocus === 'Play'){
     BackgroundPanel.style.backgroundImage = "url('"+FolderSource + "bg/2542.jpg')";
 } else {
     CurrentFocus = 'Playing';
+    Debug("this source..."+src)
     Debug(Libraries['MoviesSource']);
     Debug(MoviesList[MovieBox.id].FLDR );
     Debug(MoviesList[MovieBox.id].FILE);
     
-    Debug("this source..."+src)
+   
     PlayVideo(Libraries['MoviesSource'] + MoviesList[MovieBox.id].FLDR + MoviesList[MovieBox.id].FILE);
     
    
